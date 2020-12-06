4489849d764b70075574496e785fba61fd120251
ufoo68 <mitarou.yuutarou@gmail.com>
2020-12-06 19:05:25 +0900

    Implement messanger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import PubNub from 'pubnub'
+import { PubNubProvider, usePubNub } from 'pubnub-react'
+import Button from '@material-ui/core/Button'
+import Input from '@material-ui/core/Input'
+import { makeStyles } from '@material-ui/core/styles'
+import logo from './logo.png'
 
-function App() {
+const pubnub = new PubNub({
+  publishKey: process.env.REACT_APP_PUB_KEY,
+  subscribeKey: process.env.REACT_APP_SUB_KEY,
+})
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  preparedMessage: {
+    flexDirection: 'row',
+  },
+  customMessage: {
+    flexDirection: 'row',
+  },
+  logo: {
+    width: 400,
+  },
+})
+
+const simpleMessages = ['わーい！', 'すごーい！', 'たのしー！']
+
+const Messenger = () => {
+  const pubnub = usePubNub()
+  const [input, setInput] = useState('')
+  const classes = useStyles()
+
+  const sendMessage = useCallback(
+    async message => {
+      await pubnub.publish({
+        channel: 'nigiyakashi',
+        message: { message },
+      })
+
+      setInput('')
+    },
+    [pubnub, setInput]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classes.container}>
+      <img src={logo} alt='logo' className={classes.logo}/>
+      <div className={classes.preparedMessage}>
+        {simpleMessages.map(simpleMessage =>
+          <Button
+            onClick={e => {
+              e.preventDefault()
+              sendMessage(simpleMessage)
+            }}
+            variant="outlined"
+          >{simpleMessage}</Button>
+        )}
+      </div>
+      <div className={classes.customMessage}>
+        <Input
+          type="text"
+          placeholder="自由記入欄"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <Button
+          onClick={e => {
+            e.preventDefault()
+            sendMessage(input)
+          }}
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
-      </header>
+          送信
+      </Button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  return (
+    <PubNubProvider client={pubnub}>
+      <Messenger />
+    </PubNubProvider>
+  )
+}
+
+export default App
